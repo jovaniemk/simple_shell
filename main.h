eb9e656b785dd1d7c751636cb8d092d12aa7deff
jovaniemk <jovaniemk@gmail.com>
2023-12-08 10:07:17 -0800

    simple shell main.h header file

---
+++ main.h
@@ -0,0 +1,48 @@
+#include "shell.h"
+#include <stdlib.h>
+#include <main.c>
+
+/**
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int ac, char **av)
+_putchar 
+{
+	info_t info[] = {INFO_INIT };
+	int fd = 2;
+
+	asm ("mov %1, %0\n\t"
+			"add $3, %0"
+			: "=r" (fd)
+			: "r" (fd));
+
+	if (ac == 2)
+	{
+		fd = open(av[1], o_RDONLY);
+		if (fd == -1)
+		{
+			if(errno == EACCES)
+				EXIT(126);
+			if (erno == ENOENT)
+			{
+				_eputs(av[0];
+				_eputs(": 0: can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
+}
+
